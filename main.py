b3e9e40781578a418b778f110b5890ac3142d374
0Ruix <semihonur2k5@gmail.com>
2023-02-18 19:37:16 +0300

    Changed main.py
    -Added mp3 suport
    -Changed destination name to "media"
    -Fixed some minor stuff

--- main.py
+++ main.py
@@ -1,45 +1,56 @@
 from pytube import YouTube
+from pytube import Playlist
 from tkinter import *
+import os
 
-def Download(link,path):
+def download(link,path,filterAudio):
     youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution() #It doesnt get highest res. Only 720p
 
-    try:
-        youtubeObject.download(path)
-    except:
-        pass
+    if not filterAudio: #false
+        dwo = youtubeObject.streams.get_highest_resolution() #It doesnt get highest res. Only 720p
+        dwo.download(path)
+
+    else: #true
+        dwo = youtubeObject.streams.filter(only_audio=True)[0].download(path)
+        os.rename(dwo,path + "\\" + youtubeObject.title + ".mp3")
+
 
 # declare the window and its properities
 window = Tk()
-window.title("Ruix's You Tube Video Downloader v1.0")
+window.title("Ruix's You Tube Video Downloader v1.1")
 window.configure(width=500, height=500)
 window.configure(bg='#121212')
 window.iconbitmap("res/project.ico")
-
-#Canvas
-#canvas = Canvas(window,width=250,height=125)
-#canvas.pack()
+window.resizable(False,False)
 
 #Input Box & label
 entryBox = Entry(window,bg="#2c2c2c",fg="#ffffff",width=60)
 entryBoxLabel = Label(text="Youtube Link: ",bg="#121212",fg="white")
 
+#playlistBox = Entry(window,bg="#2c2c2c",fg="#ffffff",width=60)
+#playlistBoxLabel = Label(text="Playlist Link: ",bg="#121212",fg="white")
+
 locationBox = Entry(window,bg="#2c2c2c",fg="#ffffff",width=60)
 locationBoxLabel = Label(text="Download Path :",bg="#121212",fg="white")
-locationBox.insert(0,'Videos')
+locationBox.insert(0,'Media')
 
 #Button
-okButton = Button(text="Download",bg="#2c2c2c",command=lambda: Download(entryBox.get(),locationBox.get()))
+mp4Button = Button(text="Download MP4",bg="#2c2c2c",command=lambda: download(entryBox.get(),locationBox.get(),False))
+mp3Button = Button(text="Download MP3",bg="#2c2c2c",command=lambda: download(entryBox.get(),locationBox.get(),True))
 
+#Add Playlist download https://pytube.io/en/latest/user/playlist.html   
 
 #Grid
 entryBoxLabel.grid(row=0,column=0)
 entryBox.grid(row=0,column=1)
 
+#playlistBoxLabel.grid(row=1,column=0)
+#playlistBox.grid(row=1,column=1)
+
 locationBoxLabel.grid(row=1,column=0)
 locationBox.grid(row=1,column=1)
 
-okButton.grid(row=0,column=2)
+mp4Button.grid(row=0,column=2)
+mp3Button.grid(row=1,column=2)
 
 window.mainloop()
